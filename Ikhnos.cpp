f25bb12dfcc837f014b91060c3246bc9d6cb9196
Rocio-Mora <55737403+Rocio-Mora@users.noreply.github.com>
2022-10-04 16:33:02 +0200

    Add files via upload

---
+++ Ikhnos.cpp
@@ -0,0 +1,74 @@
+                             IKHNOS
+                             ======
+
+IKHNOS software was developed by the Universidad de Salamanca
+and the Universidad Complutense de Madrid for the analysis and
+registration of BSM using three dimensional models.
+
+IKHNOS allows for 3D model editing and saving to replicate the
+specimens found in the archaeological/paleontological record.
+
+IKHNOS also includes tools for BSM editing and analysis through
+its link with the IkhnosToolBox R library.
+
+
+                        VERSION and OPERATIVE SYSTEM:
+                        =============================
+
+The current version of IKHNOS is 5.0
+IKHNOS is developed for Windows Operative System
+
+
+
+                           LIBRARIES AND LANGUAGES
+                           =======================
+
+The software was developed in C++ with Qt 4.8 (on Visual Studio 2010)
+
+The libraries used an its dependencies are:
+			 PCL 1.8.0
+			 Boost 1.59
+			 Flann
+			 Glew
+			 VTK 5.8
+
+The database was created in SQL using XAMPP.
+
+Statistical applications were programmed in R.
+The statistical libraries used as dependencies include:
+			Rgl
+			plot3Drgl
+			spatstat
+			biwavelet
+			tidyverse
+			ggplot2
+			RMySQL
+			Rtsne
+			Rcpp
+			Circular
+			Abind
+			Factoextra
+			gridExtra
+			alphahull
+			RColorBrewer
+			Fpc
+			sm
+
+
+
+                                 DEVELOPERS
+                                 ==========
+
+Rocío Mora Fernández de Córdoba --> rociomora@usal.es
+Julia Aramendi Picado --> juliaram@usal.es
+Lloyd A. Courtenay --> ladc1995@gmail.com
+
+
+                               COLLABORATORS
+                               =============
+
+Diego Prieto Herráez --> dpriher@usal.es
+Jose Yravedra Sainzde los Terreros --> joyravedra@hotmail.com
+Miguel Ángel Maté González --> mategonzalez@usal.es
+Diego González Aguilera --> daguilera@usal.es
+
